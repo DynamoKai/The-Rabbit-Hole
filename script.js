d912f98d5a5fe9aae0cd78c08736080bfeb336fe
Dynamo <dynamokaizen@gmail.com>
2026-05-29 11:52:17 -0700

    fix attempt CE

--- script.js
+++ script.js
@@ -162,19 +162,21 @@ function displayEntries(filter = "") {
     .map((e) => {
       // PARSER: This finds [words]{content} and turns it into a clickable span
       const parsedNotes = e.notes
-        .replace(
-          // // Replaced (.*?) with ([\s\S]*?) for the content section to fix saving with whitespace
-          /\[(.*?)\]\{([\s\S]*?)\}/g,
-          (match, phrase, content) => {
-            // added .replace(/\n/g, "\\n") will prevent breaks from crashing the button(or cursor click)
-            const safeContent = content
-              .replace(/'/g, "&apos;")
-              .replace(/"/g, "&quot;")
-              .replace(/\n/g, "\\n")
-              .replace(/\r/g, "");
-            return `<span class="anecdote-link" onclick="openAnecdote('${phrase}', '${safeContent}', ${e.timestamp})">${phrase}</span>`;
-          },
-        )
+        // First Pass: Anecdote Links
+        .replace(/\[(.*?)\]\{([\s\S]*?)\}/g, (match, phrase, content) => {
+          const safePhrase = phrase
+            .replace(/'/g, "&apos;")
+            .replace(/"/g, "&quot;");
+          const safeContent = content
+            .replace(/\\/g, "\\\\")
+            .replace(/'/g, "&apos;")
+            .replace(/"/g, "&quot;")
+            .replace(/\{/g, "&#123;")
+            .replace(/\n/g, "&#125;")
+            .replace(/\n/g, "\\n")
+            .replace(/\r/g, "");
+          return `<span class="anecdote-link" onclick="openAnecdote('${phrase}', '${safeContent}', ${e.timestamp})">${phrase}</span>`;
+        })
         // The Second Pass: Cheshire Encryption
         .replace(
           /\{\{([\s\S]*?)\}\}/g,
